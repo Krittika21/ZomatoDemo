444d2a430ecd01a76b5e0016f05d17a1d09b4e8c
Krittika <Krittika@DESKTOP-TLP8QUF>
2019-09-14 14:49:41 +0530

    repository class

---
+++ ZomatoDemo.Repository/Restaurants/RestaurantRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZomatoDemo.Repository.Restaurants
+{
+    class RestaurantRepository
+    {
+    }
+}
---
+++ ZomatoDemo.Repository/Users/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZomatoDemo.Repository.Users
+{
+    interface IUserRepository
+    {
+    }
+}
