c743bc38ec733907e0ce25a0547457e8ecba0ba1
Krittika <Krittika@DESKTOP-TLP8QUF>
2019-09-16 09:47:48 +0530

    repository

--- ZomatoDemo.Repository/Restaurants/RestaurantRepository.cs
+++ ZomatoDemo.Repository/Restaurants/RestaurantRepository.cs
@@ -1,10 +1,50 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ZomatoDemo.Repository.Restaurants
 {
-    class RestaurantRepository
+    public class RestaurantRepository : IRestaurantRepository
     {
+        // GET: api/Restaurants
+        [HttpGet]
+        public IAsyncResult GetRestaurant()
+        {
+            throw new NotImplementedException();
+            //return OK();
+        }
+
+        [HttpPut]
+        public IAsyncResult PutRestaurant()
+        {
+            throw new NotImplementedException();
+            //return OK();
+
+        }
+        [HttpPost]
+        public IAsyncResult PostLocation()
+        {
+            throw new NotImplementedException();
+            //return OK();
+        }
+        [HttpPost]
+        public IAsyncResult PostRestaurants()
+        {
+            throw new NotImplementedException();
+            //return OK();
+        }
+        [HttpPost]
+        public IAsyncResult PostOrderDetails()
+        {
+            throw new NotImplementedException();
+            //return OK();
+        }
+        [HttpDelete]
+        public IAsyncResult DeleteRestaurant()
+        {
+            throw new NotImplementedException();
+            //return OK();
+        }
     }
 }
--- ZomatoDemo.Repository/Users/UserRepository.cs
+++ ZomatoDemo.Repository/Users/UserRepository.cs
@@ -1,10 +1,36 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ZomatoDemo.Repository.Users
 {
-    class UserRepository
+    public class UserRepository: IUserRepository
     {
+        [HttpGet]
+        public IAsyncResult GetUser()
+        {
+            throw new NotImplementedException();
+            //return OK();
+        }
+        [HttpPost]
+        public IAsyncResult PostUser()
+        {
+            throw new NotImplementedException();
+            //return OK();
+        }
+        [HttpPut]
+        public IAsyncResult PutUser()
+        {
+            throw new NotImplementedException();
+            //return OK();
+        }
+        [HttpDelete]
+        public IAsyncResult DeleteUser()
+        {
+            throw new NotImplementedException();
+            //return OK();
+        }
     }
 }
